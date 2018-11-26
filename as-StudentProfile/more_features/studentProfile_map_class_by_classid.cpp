1a1585c04a0f374e7b74ea812185a6c9f0354187
Gavin <gavin66@connect.hku.hk>
2018-11-26 18:15:38 +0800

    executable

--- as-StudentProfile/more_features/studentProfile_map_class_by_classid.cpp
+++ as-StudentProfile/more_features/studentProfile_map_class_by_classid.cpp
@@ -56,56 +56,78 @@ bool Database::add_profile(string id, string name, string phone){
   }
   return true;
 }
+
 bool Database::del_profile(string id){
+
+  // if the student exits
   if ( studentData.count(id) > 0 ){
-    string name = studentData[id].name;
+
+    // delete him from the student profile data
     studentData.erase(id);
+
+    // to find this student by matching his uid in class profile data
     map < string , vector<Grade> > :: iterator itrMap;
     for ( itrMap = classData.begin() ; itrMap != classData.end() ; itrMap++ ){
+
+      // now we are in a specific class, say comp2123, the searching inside the student enrolled in this course
       vector <Grade> :: iterator itrVectorGrade;
       for ( itrVectorGrade = (*itrMap).second.begin() ; itrVectorGrade != (*itrMap).second.end() ; itrVectorGrade++ ){
+
+        // now his is found, by his uid
         if ( (*itrVectorGrade).studentID == id ){
+          // delete this student from the enrolled students i.e., from <Grade> of comp2123
           (*itrMap).second.erase(itrVectorGrade);
           break;
         }
       }
     }
     return true;
-  } else {
+  }
+
+  // if the student does not exit, then show "Record Not Exists" and return false
+  else {
     cout << "Record Not Exists" << endl;
     return false;
   }
 }
 
 bool Database::add_grade(string id, string class_id, double grade){
+
+  // if this student exists in the student profile database and  he also enrolled in comp2123
   if ( studentData.count(id) > 0 ){
-    if ( classData.count(class_id) > 0 ){
-      vector <Grade> :: iterator itrVectorGrade;
-      for ( itrVectorGrade = classData[class_id].begin() ; itrVectorGrade != classData[class_id].end() ; itrVectorGrade++ ){
-        if ( (*itrVectorGrade).studentID == id ){ // student record in this course existed before -> then updated
-          (*itrVectorGrade).studentID = id;
-          (*itrVectorGrade).classID = class_id;
-          (*itrVectorGrade).mark = grade;
-          cout << "Grade Updated" << endl;
-          break;
-        }
-        if ( itrVectorGrade == classData[class_id].end() ){ // if record is new
-          Grade g;
-          g.studentID = id;
-          g.classID = class_id;
-          g.mark = grade;
-          classData[class_id].push_back(g);
-        }
-        return true;
+    vector <Grade> :: iterator itrVectorGrade;
+    bool his_is_newguy = true;
+    for ( itrVectorGrade = classData[class_id].begin() ; itrVectorGrade != classData[class_id].end() ; itrVectorGrade++ ){
+      if ( (*itrVectorGrade).studentID == id ){ // student record in this course existed before -> then updated
+        (*itrVectorGrade).studentID = id;
+        (*itrVectorGrade).classID = class_id;
+        (*itrVectorGrade).mark = grade;
+        cout << "Grade Updated" << endl;
+        his_is_newguy = false;
+        break;
       }
     }
-  } else {
+    if ( his_is_newguy ){ // if student did not ever enrolled in comp2123 before
+        Grade g;
+        g.studentID = id;
+        g.classID = class_id;
+        g.mark = grade;
+        classData[class_id].push_back(g);
+    }
+    return true;
+  }
+
+  // if his does not exist, show "Student Not Exists" and return false
+  else {
     cout << "Student Not Exists" << endl;
     return false;
   }
 }
 
+// del_grade to delete his grade or himself?
 bool Database::del_grade(string id, string class_id){
+
+
   if ( classData.count(class_id) > 0 ){
     vector <Grade> :: iterator itrVectorGrade;
     for ( itrVectorGrade = classData[class_id].begin() ; itrVectorGrade != classData[class_id].end() ; itrVectorGrade++ ){
@@ -121,8 +143,6 @@ bool Database::del_grade(string id, string class_id){
   }
 }
 
-
-//Display data
 void Database::display_profiles(){
   vector < string > v;
   map < string , Profile > :: iterator itr;
@@ -143,35 +163,33 @@ void Database::display_profiles(){
 }
 
 void Database::display_grades(string class_id){
-  double min , max , avg , count = 0;
+
+  double min , max , avg;
+  double count=0;
   double sum = 0;
-  string name;
+
   map < double , string > tempMap;
-  vector < Grade > :: iterator itrVectorGrade;
-//   cout << "HERE " << endl;
+  vector <Grade> :: iterator itrVectorGrade;
 
-  for (  itrVectorGrade = classData[class_id].begin() ; itrVectorGrade != classData[class_id].end() ; itrVectorGrade++ ){
+  min = (*classData[class_id].begin()).mark;
+  max = (*classData[class_id].begin()).mark;
 
-    if ( itrVectorGrade == classData[class_id].begin() ){
-      min = (*itrVectorGrade).mark;
-      max = (*itrVectorGrade).mark;
-    }
+  for ( itrVectorGrade = classData[class_id].begin() ; itrVectorGrade != classData[class_id].end() ; itrVectorGrade++ ){
+    string tmpString = "";
 
-    string information = "";
     map < string , Profile > :: iterator itrSystem; // searching the name
     for ( itrSystem = studentData.begin() ; itrSystem != studentData.end() ; itrSystem++ ){
       if ( (*itrSystem).first ==  (*itrVectorGrade).studentID ){
-        string name = (*itrSystem).second.name;
+        tmpString += (*itrSystem).second.name;
+        break;
       }
     }
-    information += name;
-    information += " ";
-    information += (*itrVectorGrade).studentID;
-    cout << "HERE " << information << endl;
-    tempMap[ (*itrVectorGrade).mark ] = information;
+    tmpString += " ";
+    tmpString += (*itrVectorGrade).studentID;
+    tempMap[(*itrVectorGrade).mark] = tmpString;
 
-    if ( (*itrVectorGrade).mark < min ) { min = (*itrVectorGrade).mark; }
-    if ( (*itrVectorGrade).mark > max ) { max = (*itrVectorGrade).mark; }
+    if ( (*itrVectorGrade).mark <= min ) { min = (*itrVectorGrade).mark; }
+    if ( (*itrVectorGrade).mark >= max ) { max = (*itrVectorGrade).mark; }
 
     sum += (*itrVectorGrade).mark;
     count++ ;
